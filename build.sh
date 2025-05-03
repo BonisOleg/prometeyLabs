35fca414bf213bc7ea5405635c102fe79b88a669
BonisOleg <olegbonislav@gmail.com>
2025-05-03 22:39:09 +0300

    Apdate build 1

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemessages -l uk -l en
 python manage.py compilemessages
 python manage.py collectstatic --no-input
 python manage.py migrate 
